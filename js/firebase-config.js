ecc1263c4956c5d90507d16bf965ce5c85f94c3f
frlaurenpumas <frlauren.pumas@gmail.com>
2026-08-24 14:11:03 +0200

    Update firebase-config.js
    Ajout de la connexion au Firestore (l16 à 24)

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -13,14 +13,15 @@ import {
   getDocs 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "votre-app.firebaseapp.com",
-  projectId: "votre-app-id",
-  storageBucket: "votre-app.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "XXXXXX"
-};
+const firebaseConfig = { 
+  apiKey: "AIzaSyANq5OTGWebaQlXk9wBLI9DXO21T_KZQxo", 
+  authDomain: "pumasinventory2.firebaseapp.com", 
+  projectId: "pumasinventory2", 
+  storageBucket: "pumasinventory2.firebasestorage.app", 
+  messagingSenderId: "676551378696", 
+  appId: "1:676551378696:web:a0f95c62c1b35b5f2c8ebd", 
+  measurementId: "G-K0HTSGSE9W" 
+}; 
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
